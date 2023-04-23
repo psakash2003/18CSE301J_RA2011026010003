811cf9c04ecbd628b5fc269cb3b93b8ddde6b6ba
Akash PS <100912220+psakash2003@users.noreply.github.com>
2023-04-23 16:51:07 +0530

    Create d3.js

---
+++ d3.js
@@ -0,0 +1,71 @@
+const data = [
+    { stateName: 'Andhra Pradesh', useCount: 11 },
+    { stateName: 'Assam', useCount: 4 },
+    { stateName: 'Bihar', useCount: 2 },
+    { stateName: 'Chandigarh', useCount: 5 },
+    { stateName: 'Chattisgarh', useCount: 10 },
+    { stateName: 'Delhi', useCount: 3 },
+    { stateName: 'Goa', useCount: 7 },
+  ];
+  
+  const width = 900;
+  const height = 450;
+  const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+  
+  const svg = d3.select('#d3-container')
+    .append('svg')
+    .attr('width', width - margin.left - margin.right)
+    .attr('height', height - margin.top - margin.bottom)
+    .attr("viewBox", [0, 0, width, height]);
+  
+  const x = d3.scaleBand()
+    .domain(d3.range(data.length))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+  
+  const y = d3.scaleLinear()
+    .domain([0, 14])
+    .range([height - margin.bottom, margin.top])
+  
+  svg
+    .append("g")
+    .attr("fill", 'blue')
+    .selectAll("rect")
+    .data(data.sort((a, b) => d3.descending(a.useCount, b.useCount)))
+    .join("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("y", d => y(d.useCount))
+      .attr('title', (d) => d.useCount)
+      .attr("class", "rect")
+      .attr("height", d => y(0) - y(d.useCount))
+      .attr("width", x.bandwidth());
+  svg.append("text")
+  .attr("class", "x label")
+  .attr("text-anchor", "middle")
+ .attr("fill","white")
+.attr("font-size", 20)
+  .attr("transform", "translate(" + (width / 2) + "," + (height + margin.bottom -52) + ")")
+  .text("STATES");
+
+  svg.append("text")
+  .attr("class", "y label")
+ .attr("fill","white")
+.attr("font-size", 20)
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90) translate(" + (-height/2) + "," + (-margin.left + 20) + ")")
+  .text("AVERAGEUSAGE");
+  function yAxis(g) {
+    g.attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y).ticks(null, data.format))
+      .attr("font-size", '20px')
+  }
+  
+  function xAxis(g) {
+    g.attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => data[i].stateName))
+      .attr("font-size", '20px')
+  }
+  
+  svg.append("g").call(xAxis);
+  svg.append("g").call(yAxis);
+  svg.node();
