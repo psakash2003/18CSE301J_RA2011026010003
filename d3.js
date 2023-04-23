f49a9f4ada4bfcf1f5d1e8b2529cb15c85deeee6
Akash PS <100912220+psakash2003@users.noreply.github.com>
2023-04-23 17:14:40 +0530

    Update d3.js

--- d3.js
+++ d3.js
@@ -9,7 +9,7 @@ const data = [
   ];
   
   const width = 900;
-  const height = 5000;
+  const height = 900;
   const margin = { top: 50, bottom: 50, left: 50, right: 50 };
   
   const svg = d3.select('#d3-container')
@@ -21,10 +21,10 @@ const data = [
   const x = d3.scaleBand()
     .domain(d3.range(data.length))
     .range([margin.left, width - margin.right])
-    .padding(0.1)
+    .padding(0.01)
   
   const y = d3.scaleLinear()
-    .domain([0, 100])
+    .domain([0, 20])
     .range([height - margin.bottom, margin.top])
   
   svg
